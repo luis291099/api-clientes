6479f9ebc2b3a808badcb81157a64af3cd595332
Luis Otavio Bernardo de Andrade <luis291099@gmail.com>
2021-06-09 23:16:24 -0300

    Adição das validações de celular e cpf/script para popular o banco

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,11 +8,13 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':'O cpf deve ter 11 dígitos'})
+            raise serializers.ValidationError({'cpf':'O número de CPF inserido é inválido'})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':'Não inclua números neste campo'})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':'O rg deve ter 9 dígitos'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O celular deve seguir esse modelo: xx xxxxx-xxxx'})
         return data
     # def validate_cpf(self, cpf):
     #     if len(cpf) != 11:
@@ -28,5 +30,5 @@ class ClienteSerializer(serializers.ModelSerializer):
     #     return rg
     # def validate_celular(self, celular):
     #     if len(celular) < 11:
-    #        raise serializers.ValidationError('o celular deve ter 11 dígitos')
+    #        raise serializers.ValidationError('O celular deve ter 11 dígitos')
     #     return celular 
