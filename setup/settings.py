a6e134fb5be100d106b293f441bc8932f9829296
Luis Otavio Bernardo de Andrade <luis291099@gmail.com>
2021-06-11 20:06:06 -0300

    Adição de ordenação e busca (nome, cpf)

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'clientes',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
